8ef15e468c540dcceb3d407de65f5adf00eb16a2
André <andreglock@gmail.com>
2021-05-05 09:02:42 +0200

    fixed the sales per day issue

--- index.js
+++ index.js
@@ -8,35 +8,39 @@ console.log(Math.round(Math.random() * 650 + 50));
 
 const makeData = (year, month, dayStart, dayEnd, salespersonsArr) => {
     const dataArray = [];
-    let sales = Math.round(Math.random() * 650 + 50);
-    for (i = 0; i < sales; i++) {
-        let hour = Math.round(Math.random() * 10 + 8);
-        let min = Math.round(Math.random() * 60);
-        // Transform min to string
-        if (min < 10) {
-            min = '0' + min.toFixed(0);
-        } else {
-            min = min.toFixed(0);
+    const periodDays = dayEnd - dayStart;
+    for (let i = 0; i <= periodDays; i++) {
+        let day = dayStart + i;
+        let sales = Math.round(Math.random() * 650 + 50);
+        for (j = 0; j < sales; j++) {
+            let hour = Math.round(Math.random() * 10 + 8);
+            let min = Math.round(Math.random() * 60);
+            // Transform min to string
+            if (min < 10) {
+                min = '0' + min.toFixed(0);
+            } else {
+                min = min.toFixed(0);
+            }
+            // Transform 60 min to 1 hour
+            if (min === '60') {
+                min = '00'
+                hour++;
+            }
+            // Transform hour to string
+            if (hour < 10) {
+                hour = '0' + hour.toFixed(0);
+            } else {
+                hour = hour.toFixed(0);
+            }
+            
+            let salespersons = salespersonsArr[
+                Math.floor(salespersonsArr.length - Math.random() * salespersonsArr.length)
+            ];
+            dataArray.push({
+                time: `${hour}:${min} ${day}.${month}.${year}`,
+                totalPrice: `€ ${(Math.random() * 99.5 + 0.5).toFixed(2)} salespersons: ${salespersons}`
+            })
         }
-        // Transform 60 min to 1 hour
-        if (min === '60') {
-            min = '00'
-            hour++;
-        }
-        // Transform hour to string
-        if (hour < 10) {
-            hour = '0' + hour.toFixed(0);
-        } else {
-            hour = hour.toFixed(0);
-        }
-        let day = Math.round(Math.random() * (dayEnd - dayStart) + dayStart)
-        let salespersons = salespersonsArr[
-            Math.floor(salespersonsArr.length - Math.random() * salespersonsArr.length)
-        ];
-        dataArray.push({
-            time: `${hour}:${min} ${day}.${month}.${year}`,
-            totalPrice: `€ ${(Math.random() * 99.5 + 0.5).toFixed(2)} salespersons: ${salespersons}`
-        })
     }
     return dataArray;
 };
